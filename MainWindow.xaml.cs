38a185dc26f4db83731d165100315a8aa876aa09
yulya-m <markovayulya0@yandex.ru>
2026-07-05 23:02:40 +0500

    Проработанная структура и реализованное главное окно. Маркова

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,12 @@ namespace TaskManager_Markova
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		public static MainWindow init;
 		public MainWindow()
 		{
 			InitializeComponent();
+			init = this;
+			DataContext = new VM_Pages();
 		}
 	}
 }
