e76c9955507f4ea49a7574d3d3c1c0d9696a8de6
yulya-m <markovayulya0@yandex.ru>
2026-07-06 03:07:07 +0500

    Разработанная модель задач. Маркова

---
+++ Models/Tasks.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using Schema = System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+using TaskManager_Markova.Classes;
+
+namespace TaskManager_Markova.Models
+{
+	public class Tasks : Notification
+	{
+		public int Id { get; set; }
+		private string name;
+		public string Name
+		{
+			get
+			{
+				return name;
+			}
+			set
+			{
+				Match match = Regex.Match(value, "^.{1,50}$");
+				if (!match.Success)
+					MessageBox.Show("Наименование не должно быть пустым, и не более 50 символов.",
+						"Не корректный ввод значения.");
+				else
+				{
+					name = value;
+					OnPropertyChanget("Name");
+				}
+			}
+		}
+		private string priority;
+		public string Priority
+		{
+			get
+			{
+				return priority;
+			}
+			set
+			{
+				Match match = Regex.Match(value, "^.{1,30}$");
+				if (!match.Success)
+					MessageBox.Show("Приоритет не должно быть пустым, и не более 30 символов.",
+						"Не корректный ввод значения.");
+				else
+				{
+					priority = value;
+					OnPropertyChanget("Priority");
+				}
+			}
+		}
+		private DateTime dateExecute;
+		public DateTime DateExecute
+		{
+			get
+			{
+				return dateExecute;
+			}
+			set
+			{
+				if (value.Date < DateTime.Now.Date)
+					MessageBox.Show("Дата выполнения не может быть меньше текущей.",
+						"Не корректный ввод значения.");
+				else
+				{
+					dateExecute = value;
+					OnPropertyChanget("DateExecute");
+				}
+			}
+		}
+		private string comment;
+		public string Comment
+		{
+			get
+			{
+				return comment;
+			}
+			set
+			{
+				Match match = Regex.Match(value, "^.{1,1000}$");
+				if (!match.Success)
+					MessageBox.Show("Комментарий не должен быть пустым, и не более 1000 символов.",
+						"Не корректный ввод значения.");
+				else
+				{
+					comment = value;
+					OnPropertyChanget("Comment");
+				}
+			}
+		}
+		public bool done;
+		public bool Done
+		{
+			get
+			{
+				return done;
+			}
+			set
+			{
+				done = value;
+				OnPropertyChanget("Done");
+				OnPropertyChanget("IsDoneText");
+			}
+		}
+		[Schema.NotMapped]
+		private bool IsEnable;
+		[Schema.NotMapped]
+		public bool isEnable
+		{
+			get
+			{
+				return isEnable
+			}
+			set
+			{
+				isEnable = value;
+				OnPropertyChanget("IsEnable");
+				OnPropertyChanget("IsEnableText");
+			}
+		}
+		[Schema.NotMapped]
+		public string IsEnableText
+		{
+			get
+			{
+				if (IsEnable) return "Сохранить";
+				else return "Изменить";
+			}
+		}
+		[Schema.NotMapped]
+		public string IsDoneText
+		{
+			get
+			{
+				if (Done) return "Не выполнено";
+				else return "Выполнено";
+			}
+		}
+		[Schema.NotMapped]
+		public RealyCommand OnEdit
+		{
+			get
+			{
+				return new RealyCommand(obj =>
+				{
+					IsEnable = !IsEnable;
+					if (!IsEnable)
+						(MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.tasksContext.SaveChanges();
+				});
+			}
+		}
+		[Schema.NotMapped]
+		public RealyCommand OnDelete
+		{
+			get
+			{
+				return new RealyCommand(obj =>
+				{
+					if (MessageBox.Show("Вы уверены, что хотите удалить задачу?",
+						"Предупреждение", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+					{
+						(MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.Tasks.Remove(this);
+						(MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.tasksContext.Remove(this);
+						(MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.tasksContext.SaveChanges();
+					}
+				});
+			}
+		}
+		[Schema.NotMapped]
+		public RealyCommand OnDone
+		{
+			get
+			{
+				return new RealyCommand(obj =>
+				{
+					Done = !Done;
+				});
+			}
+		}
+	}
+}
