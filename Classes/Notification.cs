821234fc98fa42decb58eb599f80bd7c9dbd28a2
yulya-m <markovayulya0@yandex.ru>
2026-07-06 00:40:09 +0500

    Разработанные классы общего-пользователя. Маркова

---
+++ Classes/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_Markova.Classes
+{
+	public class Notification : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanget([CallerMemberName] string prop = "")
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(prop));
+		}
+	}
+}
---
+++ Classes/RealyCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+
+namespace TaskManager_Markova.Classes
+{
+	public class RealyCommand : ICommand
+	{
+		private Action<object> execute;
+		private Func<object, bool> canExecute;
+		public RealyCommand(Action<object> execute, Func<object, bool> canExecute = null)
+		{
+			this.execute = execute;
+			this.canExecute = canExecute;
+		}
+		public event EventHandler CanExecuteChanged
+		{
+			add
+			{
+				CommandManager.RequerySuggested += value;
+			}
+			remove
+			{
+				CommandManager.RequerySuggested -= value;
+			}
+		}
+		public bool CanExecute(object parametr)
+		{
+			return this.CanExecute == null || this.CanExecute(parametr);
+		}
+		public void Execute(object parametr) =>
+			this.execute(parametr);
+	}
+}
