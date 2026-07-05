4c64e08b3ea2a8094b5cecdabfc3f0795fafcc25
yulya-m <markovayulya0@yandex.ru>
2026-07-06 03:17:00 +0500

    Разработанная модель представления главного окна. Маркова

---
+++ ViewModels/VM_Pages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_Markova.Classes;
+
+namespace TaskManager_Markova.ViewModels
+{
+	public class VM_Pages : Notification
+	{
+		public VM_Tasks vm_tasks = new VM_Tasks();
+		public VM_Pages()
+		{
+			MainWindow.init.frame.Navigate(new View.Main(vm_tasks));
+		}
+		public RealyCommand OnClose
+		{
+			get
+			{
+				return new RealyCommand(obj =>
+				{
+					MainWindow.init.Close();
+				});
+			}
+		}
+	}
+}
